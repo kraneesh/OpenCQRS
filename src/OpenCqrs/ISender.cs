a6bbe5143672642766f58698dbaf7937a4c9eeb8
Luca Briguglia <luca.briguglia@outlook.com>
2021-10-09 12:52:09 +0100

    Add main sender

---
+++ src/OpenCqrs/ISender.cs
@@ -0,0 +1,12 @@
+﻿using OpenCqrs.Commands;
+using OpenCqrs.Queries;
+using System.Threading.Tasks;
+
+namespace OpenCqrs
+{
+    public interface ISender
+    {
+        Task Send<TCommand>(TCommand command) where TCommand : ICommand;
+        Task<TResult> Send<TResult>(IQuery<TResult> query);
+    }
+}
---
+++ src/OpenCqrs/Sender.cs
@@ -0,0 +1,28 @@
+﻿using OpenCqrs.Commands;
+using OpenCqrs.Queries;
+using System.Threading.Tasks;
+
+namespace OpenCqrs
+{
+    public class Sender : ISender
+    {
+        private readonly ICommandSender _commandSender;
+        private readonly IQuerySender _querySender;
+
+        public Sender(ICommandSender commandSender, IQuerySender querySender)
+        {
+            _commandSender = commandSender;
+            _querySender = querySender;
+        }
+
+        public async Task Send<TCommand>(TCommand command) where TCommand : ICommand
+        {
+            await _commandSender.Send(command);
+        }
+
+        public async Task<TResult> Send<TResult>(IQuery<TResult> query)
+        {
+            return await _querySender.Send(query);
+        }
+    }
+}
